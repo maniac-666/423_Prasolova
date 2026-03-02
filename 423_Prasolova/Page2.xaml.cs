cfbee38aa79f262eb0c599dda3043868e44740f7
maniac-666 <ekate@LAPTOP-Q68JF043>
2026-03-02 19:20:23 +0300

    Функционал 2 страницы.

--- 423_Prasolova/Page2.xaml.cs
+++ 423_Prasolova/Page2.xaml.cs
@@ -14,14 +14,24 @@ using System.Windows.Shapes;
 
 namespace _423_Prasolova
 {
-    /// <summary>
-    /// Логика взаимодействия для Page2.xaml
-    /// </summary>
-    public partial class Page2 : Window
+    public partial class Page2 : Page
     {
-        public Page2()
+        public Page2() => InitializeComponent();
+
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            if (double.TryParse(txtX.Text, out double x) && double.TryParse(txtY.Text, out double y))
+            {
+                double fx = rbSh.IsChecked == true ? Math.Sinh(x) : (rbSquare.IsChecked == true ? x * x : Math.Exp(x));
+                double d;
+
+                if (x > y) d = Math.Pow(fx - y, 3) + Math.Atan(fx); // [cite: 89]
+                else if (y > x) d = Math.Pow(y - fx, 3) + Math.Atan(fx);
+                else d = Math.Pow(y + fx, 3) + 0.5;
+
+                txtRes.Text = d.ToString("F4");
+            }
+            else MessageBox.Show("Некорректный ввод!");
         }
     }
 }
