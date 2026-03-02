7795af78ebb4f8f94c734c47a687747a1a55b012
maniac-666 <ekate@LAPTOP-Q68JF043>
2026-03-02 19:16:43 +0300

    Функции для первой страницы.

--- 423_Prasolova/Page1.xaml.cs
+++ 423_Prasolova/Page1.xaml.cs
@@ -14,14 +14,24 @@ using System.Windows.Shapes;
 
 namespace _423_Prasolova
 {
-    /// <summary>
-    /// Логика взаимодействия для Page1.xaml
-    /// </summary>
-    public partial class Page1 : Window
+    public partial class Page1 : Page
     {
-        public Page1()
+        public Page1() => InitializeComponent();
+
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            if (double.TryParse(txtX.Text, out double x) && double.TryParse(txtY.Text, out double y) && double.TryParse(txtZ.Text, out double z)) // [cite: 158]
+            {
+                double part1 = Math.Pow(Math.Abs(Math.Cos(x) - Math.Cos(y)), (1 + 2 * Math.Pow(Math.Sin(y), 2)));
+                double part2 = 1 + z + Math.Pow(z, 2) / 2 + Math.Pow(z, 3) / 3 + Math.Pow(z, 4) / 4;
+                txtRes.Text = (part1 * part2).ToString("F4"); 
+            }
+            else MessageBox.Show("Проверьте правильность ввода данных!");
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtX.Clear(); txtY.Clear(); txtZ.Clear(); txtRes.Clear();
         }
     }
 }
