84ac774072e0a24d63ad39bf0e49a2119085731f
maniac-666 <ekate@LAPTOP-Q68JF043>
2026-03-23 22:29:27 +0300

    Рефакторинг первой страницы.

--- 423_Prasolova/Page1.xaml.cs
+++ 423_Prasolova/Page1.xaml.cs
@@ -1,37 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace _423_Prasolova
 {
+    /// <summary>
+    /// Страница 1: Линейный алгоритм.
+    /// Вычисляет w = |cos(x)-cos(y)|^(1+2sin²y) * (1+z+z²/2+z³/3+z⁴/4)
+    /// </summary>
     public partial class Page1 : Page
     {
+        /// <summary>
+        /// Инициализация компонентов страницы.
+        /// </summary>
         public Page1() => InitializeComponent();
 
+        /// <summary>
+        /// Обработчик нажатия кнопки «Рассчитать».
+        /// Считывает x, y, z и выводит результат формулы 1.
+        /// </summary>
         private void BtnCalc_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(txtX.Text, out double x) && double.TryParse(txtY.Text, out double y) && double.TryParse(txtZ.Text, out double z)) 
+            if (double.TryParse(txtX.Text, out double x) &&
+                double.TryParse(txtY.Text, out double y) &&
+                double.TryParse(txtZ.Text, out double z))
             {
-                double part1 = Math.Pow(Math.Abs(Math.Cos(x) - Math.Cos(y)), (1 + 2 * Math.Pow(Math.Sin(y), 2)));
-                double part2 = 1 + z + Math.Pow(z, 2) / 2 + Math.Pow(z, 3) / 3 + Math.Pow(z, 4) / 4;
-                txtRes.Text = (part1 * part2).ToString("F4"); 
+                double result = Formuler.Formula1(x, y, z);
+                txtRes.Text = result.ToString("F4");
             }
-            else MessageBox.Show("Проверьте правильность ввода данных!");
+            else
+            {
+                MessageBox.Show("Проверьте правильность ввода данных!");
+            }
         }
 
+        /// <summary>
+        /// Обработчик нажатия кнопки «Очистить».
+        /// </summary>
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
             txtX.Clear(); txtY.Clear(); txtZ.Clear(); txtRes.Clear();
         }
     }
 }
+
